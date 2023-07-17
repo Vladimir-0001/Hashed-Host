928755f6341c67992ba0815a2ced90dfdeb4d5d2
Vladimir <crecker2020@gmail.com>
2023-07-17 13:38:26 -0400

    bro what is actually happening

--- server.py
+++ server.py
@@ -1,14 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, Blueprint
+from pymongo import MongoClient
+import json 
+import os 
+
 
 app = Flask(__name__)
+client = MongoClient(os.getenv("MONGO_CONNECTION_STRING"))
+
+
+v1 = Blueprint('v1', __name__, prefix = '/api/v1')
 
 @app.route('/')
 def home():
     #i love sex squishey this function makes me want to have sex 
     return render_template('index.html')
 
-@app.route('/api/v1/upload')
+@v1.route('/upload')
 def upload():
+    api_key = request.headers.get('api_key')
+    filename = request.headers.get('filename')
+    if "Cf-Connecting-IP" in request.headers:
+        ip = request.headers["Cf-Connecting-IP"]
+    else:
+        ip = request.remote_addr
+    if request.headers.get('ApiKey') is None:
+        return json.dumps("{'error : 'ApiKey not provided'}"), 400
+    if request.args.get('filename') == None:
+        return "{'error' : 'filename is blank'}",400
+    
     #im sex
     return "file_uploaded"
 
